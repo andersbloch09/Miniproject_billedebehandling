89886e8e0afbcbfac07fdb935d335c13713426ab
Anders bloch lauridsen <93762569+andersbloch09@users.noreply.github.com>
2022-10-28 00:41:19 +0200

    does not work

--- Detect_squares.py/locate_meancolor.py
+++ Detect_squares.py/locate_meancolor.py
@@ -1,25 +1,25 @@
 import numpy as np 
-import cv2 as cv  
+import cv2 as cv
+import time 
 
-img = cv.imread("King Domino dataset/Cropped and perspective corrected boards/4.jpg",1)
+img = cv.imread(r"King Domino dataset\Cropped and perspective corrected boards\4.jpg",1)
 
 mean_array = np.zeros((5, 5, 3), dtype='uint8')
-
 board_size = 5
 
 def mean_cal():
     for j in range(board_size):
         for i in range(board_size):
-            for y in range(j*100, 100+j*100):
-                for x in range(i*100, 100+i*100): 
-                   print(x,y)
+            for y in range(j*100, j*100+100):
+                for x in range(i*100, i*100+100): 
+                   print(x,y) 
                    b = img[i,j,0]
                    g = img[i,j,1]
                    r = img[i,j,2]
-                   b = b + b.mean()
-                   g = g + g.mean()
-                   r = r + r.mean()
-                   mean_array[i,j]=(b,g,r)
+                   b_mean = b.mean
+                   g_mean = g.mean
+                   r_mean = r.mean
+            mean_array[j,i] = (b_mean, g_mean, r_mean)
 
     return(mean_array)
 
@@ -27,6 +27,6 @@ mean_board = mean_cal()
 mean_resized = cv.resize(mean_board, [500,500], interpolation = cv.INTER_AREA)
 
 cv.imshow("mean_resized", mean_resized)
-#cv.imshow("img",img)
+cv.imshow("img",img)
 cv.waitKey()
 cv.destroyAllWindows()
