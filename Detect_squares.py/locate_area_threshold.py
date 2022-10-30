50240a8335d014bf5be061888f6a8d8454a5fecd
Anders bloch lauridsen <93762569+Andersbloch69@users.noreply.github.com>
2022-10-30 09:37:55 +0100

    finding mean works

--- Detect_squares.py/locate_area_threshold.py
+++ Detect_squares.py/locate_area_threshold.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np 
 
-board_4 = cv.imread(r"King Domino dataset\Cropped and perspective corrected boards\4.jpg",1)
+board_4 = cv.imread(r"King Domino dataset/Cropped and perspective corrected boards/4.jpg",1)
 board_4_resize_small = cv.resize(board_4, [40,40], interpolation = cv.INTER_AREA)
 (y,x,channels) = board_4.shape
 print(y,x,channels)
--- Detect_squares.py/locate_meancolor.py
+++ Detect_squares.py/locate_meancolor.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import cv2 as cv
-import time 
+import statistics as sta
 
 img = cv.imread(r"King Domino dataset/Cropped and perspective corrected boards/4.jpg",1)
 
@@ -10,15 +10,21 @@ board_size = 5
 def mean_cal():
     for j in range(board_size):
         for i in range(board_size):
-            for y in range(j*100, j*100+100):
-                for x in range(i*100, i*100+100): 
+            b_mean = []
+            g_mean = []
+            r_mean = []
+            for y in range(j*100, 100+j*100):
+                for x in range(i*100, 100+i*100): 
                    print(x,y) 
                    b = img[x,y,0]
                    g = img[x,y,1]
                    r = img[x,y,2]
-                   b_mean = b.mean()
-                   g_mean = g.mean()
-                   r_mean = r.mean()
+                   b_mean.append(b)
+                   g_mean.append(g)
+                   r_mean.append(r)
+            b_mean = sta.mean(b_mean)
+            g_mean = sta.mean(g_mean)
+            r_mean = sta.mean(r_mean)
             mean_array[i,j] = (b_mean, g_mean, r_mean)
 
     return(mean_array)
