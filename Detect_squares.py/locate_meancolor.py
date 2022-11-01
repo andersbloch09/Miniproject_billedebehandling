c91c622dbf635a79ffaf6839de394c01b57cae9a
Anders bloch lauridsen <93762569+Andersbloch69@users.noreply.github.com>
2022-11-01 11:33:14 +0100

    it works

--- Detect_squares.py/locate_meancolor.py
+++ Detect_squares.py/locate_meancolor.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import statistics as sta
 
-img = cv.imread(r"King Domino dataset/Cropped and perspective corrected boards/14.jpg",1)
+img = cv.imread(r"King Domino dataset/Cropped and perspective corrected boards/23.jpg",1)
 board_size = 5
 
 def mean_cal():
@@ -30,7 +30,6 @@ def mean_cal():
 
 mean_board = mean_cal()
 
-#cv.imwrite("mean_board_25.png",mean_board)
 
 def find_landscape():
     th_corn = [(0,31),(120,178),(140,220)]
@@ -38,7 +37,7 @@ def find_landscape():
     th_meadow = [(7,48),(94,160),(77,140)]
     th_forrest = [(10,100),(34,80),(28,80)]
     th_mine = [(30,47),(53,75),(60,92)]
-    th_swamp = [(43,80),(88,120),(100,130)]
+    th_swamp = [(41,80),(80,120),(90,130)]
     landscape_map = np.zeros((5, 5),dtype='U')
     for j in range(board_size):
         for i in range(board_size):
@@ -51,10 +50,20 @@ def find_landscape():
                 landscape_map[i,j] = "ocean"
             elif b >= th_meadow[0][0] and b <= th_meadow[0][1] and g >= th_meadow[1][0] and g <= th_meadow[1][1] and r >= th_meadow[2][0] and r <= th_meadow[2][1]:
                 landscape_map[i,j] = "meadow"
+                if g < r: 
+                    landscape_map[i,j] = "not figured"
+                else: 
+                    landscape_map[i,j] = "meadow"
             elif b >= th_mine[0][0] and b <= th_mine[0][1] and g >= th_mine[1][0] and g <= th_mine[1][1] and r >= th_mine[2][0] and r <= th_mine[2][1]:
-                landscape_map[i,j] = "digging"
+                if g - b > 30: 
+                    landscape_map[i,j] = "swamp"
+                else:
+                    landscape_map[i,j] = "digging"
             elif b >= th_forrest[0][0] and b <= th_forrest[0][1] and g >= th_forrest[1][0] and g <= th_forrest[1][1] and r >= th_forrest[2][0] and r <= th_forrest[2][1]:
-                landscape_map[i,j] = "forrest"
+                if r > g: 
+                    landscape_map[i,j] = "digging"
+                else:
+                    landscape_map[i,j] = "forrest"
             elif b >= th_swamp[0][0] and b <= th_swamp[0][1] and g >= th_swamp[1][0] and g <= th_swamp[1][1] and r >= th_swamp[2][0] and r <= th_swamp[2][1]:
                 landscape_map[i,j] = "swamp"
             else:
