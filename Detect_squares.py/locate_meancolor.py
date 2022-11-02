44d55b143ef7f9b6bc9d08b80d860ee4b4a78115
Anders bloch lauridsen <93762569+andersbloch09@users.noreply.github.com>
2022-11-02 04:03:51 +0100

    works almost

--- Detect_squares.py/locate_meancolor.py
+++ Detect_squares.py/locate_meancolor.py
@@ -31,13 +31,22 @@ def mean_cal():
 mean_board = mean_cal()
 
 def find_landscape():
+    corn = 1
+    ocean = 2
+    meadow = 3
+    forrest = 4
+    digging = 5
+    swamp = 6
+    not_figured = 7 
+    castle = 8
     th_corn = [(0,31),(110,178),(130,220)]
     th_ocean = [(90,210),(46,150),(0,70)]
     th_meadow = [(7,51),(80,160),(55,145)]
     th_forrest = [(7,100),(34,80),(28,80)]
     th_mine = [(30,47),(53,76),(60,92)]
     th_swamp = [(41,95),(70,127),(84,133)]
-    landscape_map = np.zeros((5, 5),dtype='U')
+    landscape_map = np.zeros((5, 5),dtype="U")
+    area_map = np.zeros((5, 5),dtype="uint8")
     for j in range(board_size):
         for i in range(board_size):
             b,g,r = mean_board[i,j]
@@ -46,39 +55,81 @@ def find_landscape():
             if b >= th_corn[0][0] and b <= th_corn[0][1] and g >= th_corn[1][0] and g <= th_corn[1][1] and r >= th_corn[2][0] and r <= th_corn[2][1]:
                 if r - g > 30: 
                     landscape_map[i,j] = "not figured"
+                    area_map[i,j] = not_figured
                 else: 
                     landscape_map[i,j] = "corn"
+                    area_map[i,j] = corn
             elif b >= th_ocean[0][0] and b <= th_ocean[0][1] and g >= th_ocean[1][0] and g <= th_ocean[1][1] and r >= th_ocean[2][0] and r <= th_ocean[2][1]:
                 landscape_map[i,j] = "ocean"
+                area_map[i,j] = ocean
             elif b >= th_meadow[0][0] and b <= th_meadow[0][1] and g >= th_meadow[1][0] and g <= th_meadow[1][1] and r >= th_meadow[2][0] and r <= th_meadow[2][1]:
                 if g < r: 
                     if r - g > 30: 
                         landscape_map[i,j] = "not figured"
+                        area_map[i,j] = not_figured
                     else:
                         landscape_map[i,j] = "swamp"
+                        area_map[i,j] = swamp
                 else: 
                     landscape_map[i,j] = "meadow"
+                    area_map[i,j] = meadow
             elif b >= th_mine[0][0] and b <= th_mine[0][1] and g >= th_mine[1][0] and g <= th_mine[1][1] and r >= th_mine[2][0] and r <= th_mine[2][1]:
                 if g - b > 35: 
                     if r < 84:
                         landscape_map[i,j] = "forrest"
+                        area_map[i,j] = forrest
                     else:
                         landscape_map[i,j] = "swamp"
+                        area_map[i,j] = swamp
                 else:
                     landscape_map[i,j] = "digging"
+                    area_map[i,j] = digging
             elif b >= th_forrest[0][0] and b <= th_forrest[0][1] and g >= th_forrest[1][0] and g <= th_forrest[1][1] and r >= th_forrest[2][0] and r <= th_forrest[2][1]:
                 if r > 5+g: 
                     landscape_map[i,j] = "digging"
+                    area_map[i,j] = digging
                 else:
                     landscape_map[i,j] = "forrest"
+                    area_map[i,j] = forrest
             elif b >= th_swamp[0][0] and b <= th_swamp[0][1] and g >= th_swamp[1][0] and g <= th_swamp[1][1] and r >= th_swamp[2][0] and r <= th_swamp[2][1]:
                 landscape_map[i,j] = "swamp"
+                area_map[i,j] = swamp
             else:
                 landscape_map[i,j] = "not figured"
-    print(landscape_map)
+                area_map[i,j] = not_figured
+    return(area_map, landscape_map)
 
-print(mean_board)
-find_landscape()
+area_layout, landscape_map = find_landscape()
+print(landscape_map)
+print(area_layout)
+
+def locate_connections():
+    object_array = np.zeros((5, 5), dtype="uint8")
+    input_crown = area_layout[0,0] 
+    print("input_crown = ", input_crown)
+    a = 0
+    for i in range(board_size):
+        for j in range(board_size):
+            print(a)
+            print("input_crown = ", input_crown)
+            if area_layout[i,j] == input_crown:
+                object_array[i,j] = a 
+                if object_array[i-1,j] or object_array[i,j-1] == True:
+                    if object_array[i-1,j] == True:
+                        object_array[i,j] = object_array[i-1,j]
+                    elif object_array[i,j-1] == True:
+                        object_array[i,j] = object_array[i,j-1]
+                else:
+                    a += 1
+                    object_array[i,j] = a 
+            else: 
+                pass
+
+
+    print(object_array)          
+
+
+locate_connections()
 
 
 mean_resized = cv.resize(mean_board, [500,500], interpolation = cv.INTER_AREA)
