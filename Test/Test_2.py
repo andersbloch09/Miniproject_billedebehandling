d87d42d02e4c360c86e11bbbdbfcb556399614f9
Anders bloch lauridsen <93762569+Andersbloch69@users.noreply.github.com>
2022-11-09 11:05:14 +0100

    new test

--- Test/Test_2.py
+++ Test/Test_2.py
@@ -3,49 +3,20 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-board = cv.imread(r"King Domino dataset\Cropped and perspective corrected boards\9.jpg", 1)
+board = cv.imread(r"King Domino dataset/Cropped and perspective corrected boards/9.jpg", 1)
 board_gray = cv.cvtColor(board, cv.COLOR_BGR2GRAY)
-threshold = 0.60
+threshold = 0.96
 
 
-templateup = cv.imread(r'Crown_full.png',1)
+templateup = cv.imread(r'Test_pictures/Crown_full.png',0)
 templateup = cv.resize(templateup, [30,22], interpolation = cv.INTER_AREA)
-w, h, g = templateup.shape[::-1]
-res = cv.matchTemplate(board,templateup,cv.TM_CCOEFF_NORMED)
+w, h = templateup.shape[::-1]
+res = cv.matchTemplate(board_gray,templateup,cv.TM_CCORR_NORMED)
 
 loc = np.where (res >= threshold)
 for pt in zip(*loc[::-1]):
     cv.rectangle(board, pt, (pt[0] + w, pt[1] + h), (0,255,0), 2)
 
-templateleft = cv.imread(r'crown cropped 4 left.png',1)
-templateleft = cv.resize(templateleft, [22,30], interpolation = cv.INTER_AREA)
-w, h, g  = templateup.shape[::-1]
-res = cv.matchTemplate(board,templateup,cv.TM_CCOEFF_NORMED)
-
-loc = np.where (res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv.rectangle(board, pt, (pt[0] + w, pt[1] + h), (255,0,0), 2)
-
-templatedown = cv.imread(r'crown cropped 4 down.png',1)
-templatedown = cv.resize(templatedown, [30,22], interpolation = cv.INTER_AREA)
-w, h, g = templateup.shape[::-1]
-res = cv.matchTemplate(board,templateup,cv.TM_CCOEFF_NORMED)
-
-loc = np.where (res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv.rectangle(board, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-
-templateright = cv.imread(r'crown cropped 4 right.png',1)
-templateright = cv.resize(templateright, [22,30], interpolation = cv.INTER_AREA)
-w, h, g = templateup.shape[::-1]
-res = cv.matchTemplate(board,templateup,cv.TM_CCOEFF_NORMED)
-
-loc = np.where (res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv.rectangle(board, pt, (pt[0] + w, pt[1] + h), (255,0,255), 2)
-
-cv.imshow('left', templateleft)
-cv.imwrite('res.png',board)
 cv.imshow("untouched", board)
 cv.imshow("gray", board_gray)
 cv.imshow("gray_template", templateup)
